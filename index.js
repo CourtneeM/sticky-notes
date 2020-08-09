3087f9c677de3f01ec5db6371e6703d587f29c5b
Courtnee McCollum <courtneemeaghan@gmail.com>
2020-08-08 19:53:23 -0500

    Add local storage

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 let stickyNotes = {
-  notes: [{description: 'Your first note!', color: '#c465e6'}],
+  notes: [{description: "Your first note!", color: "#e66456"}],
   addNote: function(description, color) {
     this.notes.push({description, color});
+    localStorageHandler.storeNotes({description, color});
   },
   deleteNote: function(index) {
+    localStorageHandler.removeNotes(index);
     this.notes.splice(index, 1);
   }
 }
@@ -53,4 +55,76 @@ const buttonHandler = (() => {
   });
 })();
 
+
+const localStorageHandler = (() => {
+  const storeNotes = (note) => {
+    let notes;
+    if (localStorage.getItem('notes') === null) {
+      notes = stickyNotes.notes;
+    } else {
+      notes = JSON.parse(localStorage.getItem('notes'));
+      notes.push(note);
+    }
+  
+    localStorage.setItem('notes', JSON.stringify(notes));
+  };
+
+  const getNotes = () => {
+    let notes;
+    if (localStorage.getItem('notes') === null) {
+      notes = stickyNotes.notes;
+    } else {
+      notes = JSON.parse(localStorage.getItem('notes'));
+    }
+    
+    stickyNotes.notes = notes;
+    
+    const notesSection = document.querySelector('.notes');
+    if (notesSection.firstChild) {
+      while (notesSection.firstChild) {
+        notesSection.removeChild(notesSection.firstChild);
+      }
+    }
+  
+    notes.forEach((note) => {
+      let noteDiv = document.createElement('div');
+      noteDiv.style.backgroundColor = note.color;
+      notesSection.appendChild(noteDiv);
+  
+      let noteP = document.createElement('p');
+      noteP.textContent = note.description;
+      noteDiv.appendChild(noteP);
+  
+      let noteTrash = document.createElement('i');
+      noteTrash.classList.add('far', 'fa-trash-alt');
+      noteDiv.appendChild(noteTrash);
+    });
+  }
+
+  const removeNotes = (index) => {
+    let notes;
+    if (localStorage.getItem('notes') === null) {
+      notes = stickyNotes.notes;
+    } else {
+      notes = JSON.parse(localStorage.getItem('notes'));
+    }
+  
+    const noteDescription = notes[index].description;
+    notes.forEach((note) => {
+      if (note.description === noteDescription) {
+        notes.splice(index, 1);
+        localStorage.setItem('notes', JSON.stringify(notes));
+      }
+    })
+  }
+
+  return {
+    storeNotes,
+    getNotes,
+    removeNotes
+  }
+
+})();
+
+document.addEventListener('DOMContentLoaded', localStorageHandler.getNotes);
 displayNotes();
